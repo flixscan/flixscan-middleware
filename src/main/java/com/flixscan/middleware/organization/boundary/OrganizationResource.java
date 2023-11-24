69621f5c00e2557dd1fb24874d6a20ffb2cd2038
Sarwar Zahan <Sarwar.Zahan@bmw.de>
2023-11-24 11:48:53 +0100

    Feat:add organization

--- src/main/java/com/flixscan/middleware/organization/boundary/OrganizationResource.java
+++ src/main/java/com/flixscan/middleware/organization/boundary/OrganizationResource.java
@@ -1,4 +1,67 @@
+/*
+ * Copyright (c) 2024 flixscan. All rights reserved.
+ */
 package com.flixscan.middleware.organization.boundary;
 
+import java.net.URI;
+
+import com.flixscan.middleware.organization.entity.OrganizationEntity;
+import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.Response.ResponseBuilder;
+import jakarta.ws.rs.core.Response.Status;
+
+import io.smallrye.mutiny.Multi;
+import io.smallrye.mutiny.Uni;
+import io.vertx.mutiny.pgclient.PgPool;
+
+
+@Path("organization")
 public class OrganizationResource {
+
+    private final PgPool client;
+
+    public OrganizationResource(PgPool client) {
+        this.client = client;
+    }
+
+    @GET
+    public Multi<OrganizationEntity> get() {
+        return OrganizationEntity.findAll(client);
+    }
+
+    @GET
+    @Path("{id}")
+    public Uni<Response> getSingle(Long id) {
+        return OrganizationEntity.findById(client, id)
+                .onItem().transform(fruit -> fruit != null ? Response.ok(fruit) : Response.status(Status.NOT_FOUND))
+                .onItem().transform(ResponseBuilder::build);
+    }
+
+    @POST
+    public Uni<Response> create(OrganizationEntity organization) {
+        return organization.save(client)
+                .onItem().transform(id -> URI.create("/organizations/" + id))
+                .onItem().transform(uri -> Response.created(uri).build());
+    }
+
+    @PUT
+    @Path("{id}")
+    public Uni<Response> update(Long id, OrganizationEntity organization) {
+        return organization.update(client)
+                .onItem().transform(updated -> updated ? Status.OK : Status.NOT_FOUND)
+                .onItem().transform(status -> Response.status(status).build());
+    }
+
+    @DELETE
+    @Path("{id}")
+    public Uni<Response> delete(Long id) {
+        return OrganizationEntity.delete(client, id)
+                .onItem().transform(deleted -> deleted ? Status.NO_CONTENT : Status.NOT_FOUND)
+                .onItem().transform(status -> Response.status(status).build());
+    }
 }
--- src/main/java/com/flixscan/middleware/organization/entity/OrganizationEntity.java
+++ src/main/java/com/flixscan/middleware/organization/entity/OrganizationEntity.java
@@ -1,4 +1,62 @@
+/*
+ * Copyright (c) 2024 flixscan. All rights reserved.
+ */
 package com.flixscan.middleware.organization.entity;
 
+import io.smallrye.mutiny.Multi;
+import io.smallrye.mutiny.Uni;
+import io.vertx.mutiny.pgclient.PgPool;
+import io.vertx.mutiny.sqlclient.Row;
+import io.vertx.mutiny.sqlclient.RowSet;
+import io.vertx.mutiny.sqlclient.Tuple;
+
 public class OrganizationEntity {
+
+    public Long organization_id;
+
+    public String organization_name;
+
+    public OrganizationEntity() {
+        // default constructor.
+    }
+
+    public OrganizationEntity(String organization_name) {
+        this.organization_name = organization_name;
+    }
+
+    public OrganizationEntity(Long organization_id, String organization_name) {
+        this.organization_id = organization_id;
+        this.organization_name = organization_name;
+    }
+
+    public static Multi<OrganizationEntity> findAll(PgPool client) {
+        return client.query("SELECT organization_id, organization_name, organization_address FROM organization ORDER BY organization_name ASC").execute()
+                .onItem().transformToMulti(set -> Multi.createFrom().iterable(set))
+                .onItem().transform(OrganizationEntity::from);
+    }
+
+    public static Uni<OrganizationEntity> findById(PgPool client, Long id) {
+        return client.preparedQuery("SELECT organization_id, organization_name, organization_address FROM organization WHERE organization_id = $1").execute(Tuple.of(id))
+                .onItem().transform(RowSet::iterator)
+                .onItem().transform(iterator -> iterator.hasNext() ? from(iterator.next()) : null);
+    }
+
+    public Uni<Long> save(PgPool client) {
+        return client.preparedQuery("INSERT INTO organization (organization_name) VALUES ($1) RETURNING id").execute(Tuple.of(organization_name))
+                .onItem().transform(pgRowSet -> pgRowSet.iterator().next().getLong("id"));
+    }
+
+    public Uni<Boolean> update(PgPool client) {
+        return client.preparedQuery("UPDATE organization SET name = $1 WHERE id = $2").execute(Tuple.of(organization_name, organization_id))
+                .onItem().transform(pgRowSet -> pgRowSet.rowCount() == 1);
+    }
+
+    public static Uni<Boolean> delete(PgPool client, Long organization_id) {
+        return client.preparedQuery("DELETE FROM orginization WHERE organization_id = $1").execute(Tuple.of(organization_id))
+                .onItem().transform(pgRowSet -> pgRowSet.rowCount() == 1);
+    }
+
+    private static OrganizationEntity from(Row row) {
+        return new OrganizationEntity(row.getLong("organization_id"), row.getString("organization_name"));
+    }
 }
